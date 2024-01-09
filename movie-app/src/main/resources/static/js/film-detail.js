7e3035e9d0ad494774db728ed70a73fb0e4e2ca8
buihien0109 <buihien01091997@gmail.com>
2024-01-09 16:54:27 +0700

    update

--- movie-app/src/main/resources/static/js/film-detail.js
+++ movie-app/src/main/resources/static/js/film-detail.js
@@ -30,10 +30,7 @@ const formatDate = dateString => {
     const day = `0${date.getDate()}`.slice(-2);
     const month = `0${date.getMonth() + 1}`.slice(-2);
     const year = date.getFullYear();
-    const hour = `0${date.getHours()}`.slice(-2);
-    const minute = `0${date.getMinutes()}`.slice(-2);
-    const second = `0${date.getSeconds()}`.slice(-2);
-    return `${hour}:${minute}:${second} - ${day}/${month}/${year}`;
+    return `${day}/${month}/${year}`;
 }
 
 /*-------------------
